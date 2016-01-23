1b3486b55bf2d23fdc37ba7097ad3b38dae0e4d2
mikilukasik <szarhazi@gmail.com>
2016-01-23 13:26:43 +0000

    done

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require('express');
-var http = require('http')
-
 var app = express()
-var httpServ = http.createServer(app)
-
-var server = httpServ.listen(3000, function() {
-	
+var server = app.listen(3000, function() {
 	console.log('app listening');
-	
 });
-
 app.use(express.static('public'))
-
-
